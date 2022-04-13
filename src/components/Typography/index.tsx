6718bf75f6c04690b14efee8ccff722479cf378b
Daniel de Sousa Santos <daniel.sousa.developer@gmail.com>
2022-04-13 10:30:59 -0300

    feat: create typography

---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components/native';
+
+export const ScreenTitle = styled.Text`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+`;
+
+export const SectionTitle = styled.Text`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 28px;
+  margin-left: 16px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+`;
+
+export const ProductDescription = styled.Text`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 12px;
+  color: #85868a;
+  margin-top: 4px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-weight: 700;
+  font-size: 20px;
+  color: #8775fe;
+`;
+
+export const ProductTag = styled.Text`
+  font-weight: 700;
+  font-size: 8px;
+  line-height: 9px;
+  color: #8775fe;
+  margin-top: 7px;
+  margin-bottom: 7px;
+`;
